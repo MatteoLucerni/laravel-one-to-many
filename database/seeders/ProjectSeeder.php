bb1b4f5db54d5045b52294510635899776380d33
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-06 19:29:23 +0200

    added category table and seeder for it

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Project;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
@@ -15,11 +16,13 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for ($i = 0; $i < 50; $i++) {
+
+        Storage::makeDirectory('project_images');
+        for ($i = 0; $i < 30; $i++) {
             $proj = new Project();
             $proj->title = $faker->text(35);
             $proj->slug = Str::slug($proj->title, '-');
-            $proj->image = $faker->imageUrl(250, 250);
+            $proj->image = $faker->image(storage_path('app/public/project_images'), 250, 250);
             $proj->description = $faker->paragraph(30, true);
             $proj->main_lang = $faker->word();
             $proj->other_langs = $faker->words(3, true);
