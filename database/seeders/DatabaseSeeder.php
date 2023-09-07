62bb0eae6b49528532f0b8b31262f9d89b364ca2
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-07 02:20:16 +0200

    changes seeds order

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([ProjectSeeder::class, UserSeeder::class, TypeSeeder::class]);
+        $this->call([TypeSeeder::class, ProjectSeeder::class, UserSeeder::class]);
     }
 }
